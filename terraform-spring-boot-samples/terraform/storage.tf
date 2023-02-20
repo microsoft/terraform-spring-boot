f505e5f087360b70e761d25b13383d009bc7df47
Jonathan Schnéider <jkschneider@gmail.com>
2023-02-20 21:12:41 +0000

    refactor: Best practices for Azure
    Co-authored-by: Moderne <team@moderne.io>

--- terraform-spring-boot-samples/terraform/storage.tf
+++ terraform-spring-boot-samples/terraform/storage.tf
@@ -4,9 +4,15 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "testsa" {
-  name                     = "tfclienttestsa"
-  resource_group_name      = "${azurerm_resource_group.rg.name}"
-  location                 = "${azurerm_resource_group.rg.location}"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+  name                      = "tfclienttestsa"
+  resource_group_name       = "${azurerm_resource_group.rg.name}"
+  location                  = "${azurerm_resource_group.rg.location}"
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  enable_https_traffic_only = true
+  network_rules {
+    default_action = "Deny"
+  }
+  bypass                    = ["AzureServices"]
+  min_tls_version           = "TLS1_2"
 }
